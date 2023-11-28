18921b1c16f44d7df465e6fb71ec9025295b4cfc
AkshataGawade <akshatagawade25@gmail.com>
2023-11-29 10:02:41 +1100

    Updated main class to take user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,35 +12,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, JsonProcessingException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Do you want to use the default values? (y/n)");
-        String useDefault = scanner.nextLine().trim().toLowerCase();
+        while (true) {
+            System.out.println("Do you want to use the default values? (y/n)");
+            String useDefault = scanner.nextLine().trim().toLowerCase();
 
-        String a = "abc";
-        int b = 2;
+            String a = "abc";
+            int b = 2;
 
-        if (!useDefault.equals("y")) {
-            System.out.println("Enter value for 'a' (string):");
-            a = scanner.nextLine();
+            if (!useDefault.equals("y")) {
+                System.out.println("Enter value for 'a' (string):");
+                a = scanner.nextLine();
 
-            System.out.println("Enter value for 'b' (integer):");
-            while (!scanner.hasNextInt()) {
-                System.out.println("Please enter an integer value for 'b':");
-                scanner.next();
+                System.out.println("Enter value for 'b' (integer):");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Please enter an integer value for 'b':");
+                    scanner.next();
+                }
+                b = scanner.nextInt();
             }
-            b = scanner.nextInt();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            try {
+                IfStatement ifStatement = mapper.readValue(new File("condition.json"), IfStatement.class);
+                boolean result = ifStatement.evaluate(a, b);
+                System.out.println("Result: " + result);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("\nDo you want to evaluate another expression? (y/n)");
+            String continueChoice = scanner.nextLine().trim().toLowerCase();
+            if (!continueChoice.equals("y")) {
+                break;
+            }
         }
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            IfStatement ifStatement = mapper.readValue(new File("condition.json"), IfStatement.class);
-            boolean result = ifStatement.evaluate(a, b);
-            System.out.println("Result: " + result);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         scanner.close();
     }
 }
